04da77eaf50d7da91f63b5bf2394f233c6f13f7e
Simon <simon@MacBook-Pro-Simon.local>
2019-12-18 16:43:39 +0300

    [ui] remove chips in styleguide

--- styleguide/main/pages/PComponent/index.js
+++ styleguide/main/pages/PComponent/index.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import { observer } from 'startupjs'
-import { View } from 'react-native'
-import { Chips } from '@startupjs/ui'
+import { Props } from 'components'
+import { View, Text } from 'react-native'
+import propTypes from 'prop-types'
 import './index.styl'
 
 export default observer(function PComponent () {
   return pug`
     View.root
-      Chips(
-        title=123
-        iconLeft={name: '2'}
-      )
+      View.left
+        Text.text Syntax highlighter
+      View.right
+        Props(of=RenderText)
   `
 })
+
+function RenderText ({ value }) {
+  return pug`
+    Text= value
+  `
+}
+
+RenderText.propTypes = {
+  value: propTypes.string
+}
